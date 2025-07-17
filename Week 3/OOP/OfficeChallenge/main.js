70e3b0efb53e2b41b7b6f9ac0e3c6c660725ba61
Assaf Mor <asafmor14@gmail.com>
2025-07-17 18:49:03 +0300

    Implemented OfficeChallenge required classes

--- Week 3/OOP/OfficeChallenge/main.js
+++ Week 3/OOP/OfficeChallenge/main.js
@@ -1,3 +1,75 @@
 //write your code here to make the tests pass
 
-console.log("mamatzav");
+class Employee{
+    constructor(name){
+        this.name = name;
+    }
+
+    work(office){
+        for(let i = 0; i < 10; i++){
+            office.documents.push(new Document(this.name));
+        }
+    }
+}
+
+class Cleaner{
+    constructor(name){
+        this.name = name;
+    }
+
+    clean(){
+        console.log("Clean");
+
+    }
+}
+
+class Manager{
+    constructor(name){
+        this.name = name;
+        this.employees = [];
+    }
+
+    hireEmployee(name){
+        this.employees.push(new Employee(name));
+    }
+
+    askEmployeesToWork(office){
+        for(let emp of this.employees){
+            emp.work(office)
+        }
+    }
+}
+
+
+class Document{
+    constructor(EmployeeName){
+        this.EmployeeName = EmployeeName;
+    }
+}
+
+
+class Office{
+    constructor(){
+        this.documents = [];
+        this.managers = [];
+        this.cleaners = [];
+    }
+
+    hireManager(name){
+        this.managers.push(new Manager(name));
+    }
+
+    hireCleaner(name){
+        this.cleaners.push(new Cleaner(name));
+    }
+
+    startWorkDay(){
+        for(let m of this.managers){
+            m.askEmployeesToWork(this);
+        }
+
+        for(let c of this.cleaners){
+            c.clean();
+        }
+    }
+}
