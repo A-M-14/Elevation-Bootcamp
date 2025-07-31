e0199f2c61ed17e1bceaab770cbfb3e3a12bc79f
Assaf Mor <asafmor14@gmail.com>
2025-07-31 13:55:20 +0300

    Implementing Controller class

--- Week 5/RUPG Project/app.js
+++ Week 5/RUPG Project/app.js
@@ -91,5 +91,27 @@ class View {
   }
 }
 
+class Controller {
+  constructor(view) {
+    this.view = view;
+    this.view.bindGenerate(this.loadAll.bind(this));
+  }
 
+  async loadAll() {
+    try {
+      const users = await APIService.fetchRandomUsers();
+      const quote = await APIService.fetchKanyeQuote();
+      const pokemon = await APIService.fetchRandomPokemon();
+      const about = await APIService.fetchBaconIpsum();
 
+      this.view.renderProfile(users[0]);
+      this.view.renderFriends(users.slice(1));
+      this.view.renderQuote(quote);
+      this.view.renderPokemon(pokemon);
+      this.view.renderAbout(about);
+
+    } catch (err) {
+      alert(err.message);
+    }
+  }
+}
