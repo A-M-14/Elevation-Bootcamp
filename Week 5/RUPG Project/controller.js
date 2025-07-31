50c6966027bf973f28a8fa45425a5c2319024027
Assaf Mor <asafmor14@gmail.com>
2025-07-31 14:22:14 +0300

    Fixing initial loading and improving performance with Promise.all

--- Week 5/RUPG Project/controller.js
+++ Week 5/RUPG Project/controller.js
@@ -4,14 +4,19 @@ export class Controller {
   constructor(view) {
     this.view = view;
     this.view.bindGenerate(this.loadAll.bind(this));
+    
+    // Load initial data when the app starts
+    this.loadAll();
   }
 
   async loadAll() {
     try {
-      const users = await APIService.fetchRandomUsers();
-      const quote = await APIService.fetchKanyeQuote();
-      const pokemon = await APIService.fetchRandomPokemon();
-      const about = await APIService.fetchBaconIpsum();
+      const [users, quote, pokemon, about] = await Promise.all([
+        APIService.fetchRandomUsers(),
+        APIService.fetchKanyeQuote(),
+        APIService.fetchRandomPokemon(),
+        APIService.fetchBaconIpsum()
+      ]);
 
       this.view.renderProfile(users[0]);
       this.view.renderFriends(users.slice(1));
